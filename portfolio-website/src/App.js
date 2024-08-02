7c0c10383cc57d05e24fb0d51fa860a9d68f7361
Dhanawardhana G.A.C.G. <109666856+ChalanaGayan@users.noreply.github.com>
2024-08-02 23:07:29 +0530

    hello

--- portfolio-website/src/App.js
+++ portfolio-website/src/App.js
@@ -9,12 +9,7 @@ const App = () => {
     <div className="bg-gray-900 text-white">
       <Navbar />
       <Header />
-      <Section id="projects" title="My Projects" />
-      <Section id="education" title="My Education" />
-      <Section id="achievements" title="My Achievements" />
-      <Section id="interests" title="What I'm Interested In" />
-      <Section id="cv" title="My CV" />
-      <Section id="experience" title="My Experience" />
+
       <Footer />
     </div>
   );
--- portfolio-website/src/components/Header.js
+++ portfolio-website/src/components/Header.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Header = () => {
     return (
         <header className="h-screen flex items-center justify-center">
-            <h1 className="text-5xl font-bold">Chalana Gayan</h1>
-            <p className="text-2xl mt-4">Computer Science and Engineering Undergraduate</p>
+            <h1 className="text-5xl font-bold">CHALANA GAYAN</h1>
+            <p className="text-2xl mt-4">UNDER CONSTRUCTION</p>
         </header>
     );
 };
