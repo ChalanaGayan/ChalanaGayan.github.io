547bc5bdad457bb50f2fcb81273b2a0af8f85ab6
Dhanawardhana G.A.C.G. <109666856+ChalanaGayan@users.noreply.github.com>
2024-10-15 20:55:51 +0530

    new update - init

---
+++ src/components/About.jsx
@@ -0,0 +1,19 @@
+// import Tilt from "react-tilt";
+// import { motion } from "framer-motion";
+
+// import { styles } from "../styles";
+// import { services } from "../constants";
+
+// import { fadeIn, textVariant } from "../utils/motion";
+
+
+
+const About = () => {
+  return (
+    <div>
+      hellp
+    </div>
+  );
+};
+
+export default About
